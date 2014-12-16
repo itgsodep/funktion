81be02947be912120cf87453b3f211f9b907a684
edvard panes <edvard.panes@it-gymnasiet.se>
2014-12-16 16:54:07 +0100

    Ändring funktion1 och funktion2

---
+++ nummer1.rb
@@ -0,0 +1,33 @@
+puts 4 + 7
+
+puts 7 - 3
+
+puts 7 - 8
+
+puts 7 - -7
+
+puts 4 * 8
+
+puts 6/3
+
+puts 10/4
+
+puts 10.0/4
+
+puts 10/4.0
+
+puts "Is it greater? #{5 > -2}"
+
+puts 3>4
+
+puts 6>5
+
+puts 5 == 3
+
+puts "skriv in ett tal"
+tal = gets.to_i
+
+puts 2* tal
+
+puts "skriv in ett tal"
+tal = gets.to_i
